d76079b6f0687b6d8499499d670b5c61546cbbc0
ravindu-kandawela1205 <ravindu.l@botcalm.com>
2025-09-30 15:26:06 +0530

    fix(UI): roadmap section responsive and sizes fixed for  sm md xl

--- src/pages/RoadMap/Sections/Roadmap.tsx
+++ src/pages/RoadMap/Sections/Roadmap.tsx
@@ -21,10 +21,10 @@ function Roadmap() {
         {/* Mobile pyramid layout (1-2-2) */}
         <div className="mt-10 px-6 md:hidden">
           <div className="flex justify-center">
-            <img src={Pen3} alt="Road map stage 3" className="h-auto w-28 " />
+            <img src={Pen3} alt="Road map stage 3" className="h-auto w-28" />
           </div>
           <div className="mt-6 flex items-center justify-center gap-6">
-            <img src={Pen2} alt="Road map stage 2" className="h-auto w-24 " />
+            <img src={Pen2} alt="Road map stage 2" className="h-auto w-24" />
             <img src={Pen4} alt="Road map stage 4" className="h-auto w-24" />
           </div>
           <div className="mt-6 flex items-center justify-center gap-6">
@@ -35,59 +35,98 @@ function Roadmap() {
 
         {/* Desktop layout (unchanged) */}
         <div className="hidden grid-cols-5 px-30 md:grid">
+          {/* image 1 */}
+
           <div className="group -ml-40 flex flex-row justify-center">
-            <p className="lg:mt-20 lg:ml-10 xl:mt-10 2xl:mt-50 mt-50 h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:text-[15px]">
-              This is a short explanation. make it short and clear to keep
-              students
-            </p>
+            <div className="mt-50 h-40 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-4 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-60 lg:ml-10 lg:w-[200px] 2xl:h-50 lg:text-[15px] xl:mt-40 xl:h-50 2xl:mt-40">
+              <p className="font-jel text-center lg:text-[12px] xl:text-[16px]">
+                S-1
+              </p>
+              <p className="font-jel text-left lg:text-[12px] xl:text-[16px]">
+                This is a short explanation. make it short and clear to keep
+                students
+              </p>
+            </div>
             <img
               src={Pen1}
               alt=""
-              className="absolute bottom-30 group-hover:-mb-8 md:h-auto md:w-40 group-hover:h-auto group-hover:w-[180px]"
+              className="absolute bottom-30 group-hover:-mb-8 group-hover:h-auto group-hover:w-[180px] md:h-auto md:w-40"
             />
           </div>
 
+          {/* image 2 */}
+
           <div className="group flex flex-row justify-center">
-            <p className="lg:-mt-10 xl:-mt-2 2xl:mt-40 2xl:h-40 mt-40 lg:h-60 h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:text-[15px]">
-              This is a short explanation. make it short and clear to keep
-              students
-            </p>
+            <div className="mt-40 h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-4 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-40 lg:h-40 lg:w-[200px] lg:text-[15px] xl:mt-30 xl:h-50 2xl:mt-30 2xl:h-50">
+              <p className="font-jel text-center lg:text-[12px] xl:text-[16px]">
+                S-2
+              </p>
+              <p className="font-jel text-left lg:text-[12px] xl:text-[16px]">
+                This is a short explanation. make it short and clear to keep
+                students
+              </p>
+            </div>
             <img
               src={Pen2}
               alt=""
-              className="md:h-auto md:w-40 absolute bottom-[230px] ml-10 group-hover:mb-[-70px] group-hover:h-auto group-hover:w-[180px]"
+              className="absolute bottom-[230px] ml-10 group-hover:mb-[-70px] group-hover:h-auto group-hover:w-[180px] md:h-auto md:w-40"
             />
           </div>
 
+          {/* image 3 */}
+
           <div className="group flex flex-row justify-center">
-            <p className="lg:mt-10 xl:mt-5 2xl:mt-40 mt-40 h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:text-[15px]">
-              This is a short explanation. make it short and clear to keep
-              students
-            </p>
+            <div className="mt-40 h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-4 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-50 lg:h-40 lg:w-[200px] lg:text-[15px] xl:mt-30 xl:h-50 2xl:mt-30 2xl:h-50">
+              <p className="font-jel text-center lg:text-[12px] xl:text-[16px]">
+                S-3
+              </p>
+              <p className="font-jel text-left lg:text-[12px] xl:text-[16px]">
+                This is a short explanation. make it short and clear to keep
+                students
+              </p>
+            </div>
             <img
               src={Pen3}
               alt=""
-              className="md:h-auto md:w-40 absolute bottom-[200px] group-hover:mb-[-50px] group-hover:h-auto group-hover:w-[180px]"
+              className="absolute bottom-[200px] group-hover:mb-[-50px] group-hover:h-auto group-hover:w-[180px] md:h-auto md:w-40"
             />
           </div>
 
+          {/* image 4 */}
+
           <div className="group flex flex-row justify-center">
-            <p className="lg:mt-20 xl:mt-10 2xl:mt-50 mt-50 h-40 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:text-[15px]">
-              This is a short explanation. make it short and clear to keep
-              students
-            </p>
+            <div className="mt-40 h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-4 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-50 lg:h-40 lg:w-[200px] lg:text-[15px] xl:mt-40 xl:h-50 2xl:mt-35 2xl:h-50">
+              <p className="font-jel text-center lg:text-[12px] xl:text-[16px]">
+                S-4
+              </p>
+              <p className="font-jel text-left lg:text-[12px] xl:text-[16px]">
+                This is a short explanation. make it short and clear to keep
+                students
+              </p>
+            </div>
             <img
               src={Pen4}
               alt=""
-              className="md:h-auto md:w-40 absolute bottom-[200px] group-hover:mb-[-50px] group-hover:h-auto group-hover:w-[180px]"
+              className="absolute bottom-[200px] group-hover:mb-[-50px] group-hover:h-auto group-hover:w-[180px] md:h-auto md:w-40"
             />
           </div>
+
+          {/* image 5 */}
           <div className="group flex flex-row justify-center">
-            <p className="lg:mt-20 xl:mt-20 2xl:mt-60 mt-60 h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:text-[15px]">
-              This is a short explanation. make it short and clear to keep
-              students
-            </p>
-            <img src={Pen5} alt="" className="md:h-auto md:w-40 absolute bottom-30 ml-20 group-hover:-mb-8 group-hover:h-auto group-hover:w-[180px]" />
+            <div className="mt-40 h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-4 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-70 lg:h-40 lg:w-[200px] lg:text-[15px] xl:mt-50 xl:h-50 2xl:mt-40 2xl:ml-10 2xl:h-50">
+              <p className="font-jel text-center lg:text-[16px] xl:text-[24px]">
+                S-5
+              </p>
+              <p className="font-jel text-left lg:text-[12px] xl:text-[16px]">
+                This is a short explanation. make it short and clear to keep
+                students
+              </p>
+            </div>
+            <img
+              src={Pen5}
+              alt=""
+              className="absolute bottom-30 ml-20 group-hover:-mb-8 group-hover:h-auto group-hover:w-[180px] md:h-auto md:w-40"
+            />
           </div>
         </div>
       </div>
--- src/pages/Token/sections/Token.tsx
+++ src/pages/Token/sections/Token.tsx
@@ -19,41 +19,41 @@ function Token() {
           {/* left image */}
 
           <div className="group flex justify-center">
-            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-20">
+            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-30">
               <p className="font-jel">0%</p>
               <p className="font-jel">Buy Tax</p>
             </div>
             <img
               src={Toke1}
               alt="Token 1"
-              className="absolute top-120 h-auto w-[150px] sm:top-100 sm:h-auto sm:w-[300px] xl:top-[400px] 2xl:top-[500px]"
+              className="absolute top-120 h-auto w-[150px] sm:top-100 sm:h-auto sm:w-[300px] xl:top-[400px] 2xl:top-[500px] group-hover:mt-10"
             />
           </div>
 
           {/* middle image  */}
 
           <div className="group flex justify-center group">
-            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-35">
+            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-45">
               <p className="font-jel">0%</p>
               <p className="font-jel">Sell Tax</p>
             </div>
             <img
               src={Toke2}
               alt="Token 2"
-              className="absolute top-130 h-auto w-[150px] sm:top-110 sm:h-auto sm:w-[300px] xl:top-110 2xl:top-[520px]"
+              className="absolute top-130 h-auto w-[150px] sm:top-110 sm:h-auto sm:w-[300px] xl:top-110 2xl:top-[520px] group-hover:mt-10"
             />
           </div>
 
           {/* right image  */}
           <div className="flex justify-center group">
-            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-20">
+            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-30">
               <p className="font-jel">0%</p>
               <p className="font-jel">Liquiity Tax</p>
             </div>
             <img
               src={Toke3}
               alt="Token 3"
-              className="absolute top-120 h-auto w-[150px] sm:top-100 sm:h-auto sm:w-[300px] xl:top-[400px] 2xl:top-[500px]"
+              className="absolute top-120 h-auto w-[150px] sm:top-100 sm:h-auto sm:w-[300px] xl:top-[400px] 2xl:top-[500px] group-hover:mt-10"
             />
           </div>
         </div>
