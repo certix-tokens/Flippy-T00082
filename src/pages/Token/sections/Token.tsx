2255f296e65ab5a0da08019a6dad28a130d8c364
ravindu-kandawela1205 <ravindu.l@botcalm.com>
2025-10-01 09:34:57 +0530

    fix(UI): fixed token section 'sm,md,lg,xl,2xl'

--- src/pages/Token/sections/Token.tsx
+++ src/pages/Token/sections/Token.tsx
@@ -19,41 +19,41 @@ function Token() {
           {/* left image */}
 
           <div className="group flex justify-center">
-            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-30">
+            <div className="mt-90 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-40 lg:text-[15px] xl:mt-5 2xl:mt-40 md:mt-40 sm:mt-65">
               <p className="font-jel">0%</p>
               <p className="font-jel">Buy Tax</p>
             </div>
             <img
               src={Toke1}
               alt="Token 1"
-              className="absolute top-120 h-auto w-[150px] sm:top-100 sm:h-auto sm:w-[300px] xl:top-[400px] 2xl:top-[500px] group-hover:mt-10"
+              className="absolute top-145 h-auto w-[150px] sm:top-120 sm:h-auto sm:w-[250px] xl:top-[500px] 2xl:top-[550px] group-hover:mt-10 lg:top-[500px] md:top-[500px]  "
             />
           </div>
 
           {/* middle image  */}
 
           <div className="group flex justify-center group">
-            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-45">
+            <div className="mt-110 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-70 lg:text-[15px] xl:mt-60 2xl:mt-60 md:mt-70 sm:mt-90 ">
               <p className="font-jel">0%</p>
               <p className="font-jel">Sell Tax</p>
             </div>
             <img
               src={Toke2}
               alt="Token 2"
-              className="absolute top-130 h-auto w-[150px] sm:top-110 sm:h-auto sm:w-[300px] xl:top-110 2xl:top-[520px] group-hover:mt-10"
+              className="absolute top-160 h-auto w-[150px] sm:top-140 sm:h-auto sm:w-[250px] xl:top-140 2xl:top-[580px] lg:top-[550px] group-hover:mt-10 md:top-[550px]"
             />
           </div>
 
           {/* right image  */}
           <div className="flex justify-center group">
-            <div className="mt-60 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-20 lg:text-[15px] xl:mt-20 2xl:mt-30">
+            <div className="mt-65 h-30 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-30 lg:text-[15px] xl:mt-10 2xl:mt-30 md:mt-30 sm:mt-40">
               <p className="font-jel">0%</p>
               <p className="font-jel">Liquiity Tax</p>
             </div>
             <img
               src={Toke3}
               alt="Token 3"
-              className="absolute top-120 h-auto w-[150px] sm:top-100 sm:h-auto sm:w-[300px] xl:top-[400px] 2xl:top-[500px] group-hover:mt-10"
+              className="absolute top-145 h-auto w-[150px] sm:top-120 sm:h-auto sm:w-[250px] xl:top-[500px] 2xl:top-[550px] group-hover:mt-10 lg:top-[500px] md:top-[500px]"
             />
           </div>
         </div>
