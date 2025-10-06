17b7a762cfa6ec0928a27ed463a6e0c9a418c5fc
ravindu-kandawela1205 <ravindu.l@botcalm.com>
2025-10-06 16:39:12 +0530

    fix(UI): fix token page responsive and poistions

--- src/pages/Token/sections/Token.tsx
+++ src/pages/Token/sections/Token.tsx
@@ -2,9 +2,15 @@ import Tokenbg from '../../../assets/Images/tokenomicsbg.png';
 import Toke1 from '../../../assets/Images/toke1.png';
 import Toke2 from '../../../assets/Images/toke2.png';
 import Toke3 from '../../../assets/Images/toke3.png';
+import { useState } from 'react';
 function Token() {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const toggleOpacity = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
   return (
-    <div className="relative h-[86vh] overflow-hidden">
+    <div className="relative h-[87vh] overflow-y-hidden">
       <div
         className="absolute inset-0 bg-cover bg-[position:70%_center] md:bg-center"
         style={{ backgroundImage: `url(${Tokenbg})` }}
@@ -18,43 +24,50 @@ function Token() {
         <div className="mx-auto grid w-full max-w-6xl grid-cols-3 place-items-center justify-center gap-6 py-8 sm:grid-cols-3 lg:grid-cols-3">
           {/* left image */}
 
-          <div className="group flex justify-center">
-            <div className="mt-70 h-30 w-[160px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-40 lg:text-[15px] xl:mt-5 2xl:mt-40 md:mt-40 sm:mt-45">
+          <div className="group flex justify-center" onClick={() => toggleOpacity(0)}>
+            <div className={`-h-30 top-[47%] left-2 absolute rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 md:top-[40%] lg:top-[40%] lg:left-50 lg:text-[15px] sm:top-[42%] sm:left-15 xl:top-[40%] xl:left-40 xl:w-[260px] 2xl:top-[40%] 2xl:left-80 ${activeIndex === 0 ? 'opacity-100' : 'opacity-0'}`}>
               <p className="font-jel">0%</p>
               <p className="font-jel">Buy Tax</p>
             </div>
-            <img
-              src={Toke1}
-              alt="Token 1"
-              className="absolute top-145 h-auto w-[150px] sm:top-120 sm:h-auto sm:w-[250px] xl:top-[500px] 2xl:top-[550px] group-hover:mt-10 lg:top-[500px] md:top-[500px]  "
-            />
+            <div className="flex">
+              <img
+                src={Toke1}
+                alt="Token 1"
+                className="absolute h-auto w-[150px]  left-2 top-[60%] sm:w-[200px] sm:top-[55%] sm:left-10 md:left-10 md:top-[49%] md:w-[250px] lg:top-[48%] lg:left-35 md:group-hover:top-[52%] lg:group-hover:top-[50%] xl:top-[45%] xl:w-[300px] xl:group-hover:top-[50%] 2xl:top-[35%] 2xl:left-60 2xl:w-[447px]"
+              />
+            </div>
           </div>
 
           {/* middle image  */}
 
-          <div className="group flex justify-center group">
-            <div className="mt-110 h-30 w-[160px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-70 lg:text-[15px] xl:mt-60 2xl:mt-60 md:mt-70 sm:mt-90 ">
+          <div className="group group flex justify-center" onClick={() => toggleOpacity(1)}>
+            <div className={`absolute h-30 top-[55%] rounded-2xl sm:top-[50%] bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 md:top-[47%] lg:top-[48%] xl:top-[43%] xl:w-[260px] 2xl:top-[40%] ${activeIndex === 1 ? 'opacity-100' : 'opacity-0'} `}>
               <p className="font-jel">0%</p>
               <p className="font-jel">Sell Tax</p>
             </div>
-            <img
-              src={Toke2}
-              alt="Token 2"
-              className="absolute top-160 h-auto w-[150px] sm:top-140 sm:h-auto sm:w-[250px] xl:top-140 2xl:top-[580px] lg:top-[550px] group-hover:mt-10 md:top-[550px]"
-            />
+            <div className="flex justify-center">
+              <img
+                src={Toke2}
+                alt="Token 2"
+                className="absolute h-auto w-[150px] top-[65%] sm:w-[200px] sm:top-[58%] md:w-[250px] md:top-[53%] md:group-hover:top-[55%] lg:top-[52%] lg:group-hover:top-[55%] xl:top-[48%] xl:w-[300px] xl:group-hover:top-[50%] 2xl:top-[40%] 2xl:w-[447px] 2xl:group-hover:top-[45%]"
+              />
+            </div>
           </div>
 
           {/* right image  */}
-          <div className="flex justify-center group">
-            <div className="mt-60 h-30 w-[160px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:mt-30 lg:text-[15px] xl:mt-10 2xl:mt-30 md:mt-30 sm:mt-40">
+          <div className="group flex justify-center" onClick={() => toggleOpacity(2)}>
+            <div className={`absolute h-30 top-[47%] right-2 rounded-2xl sm:top-[43%] sm:right-15 bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-10 text-center text-white md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 lg:right-45 lg:top-[42%] md:right-25 md:top-[40%] lg:text-[15px] xl:top-[40%] xl:right-40 xl:w-[260px] 2xl:top-[37%] 2xl:right-60 ${activeIndex === 2 ? 'opacity-100' : 'opacity-0'}  `}>
               <p className="font-jel">0%</p>
               <p className="font-jel">Liquiity Tax</p>
             </div>
-            <img
-              src={Toke3}
-              alt="Token 3"
-              className="absolute top-145 h-auto w-[150px] sm:top-120 sm:h-auto sm:w-[250px] xl:top-[500px] 2xl:top-[550px] group-hover:mt-10 lg:top-[500px] md:top-[500px]"
-            />
+
+            <div className="flex">
+              <img
+                src={Toke3}
+                alt="Token 3"
+                className="absolute right-2 h-auto top-[60%] w-[150px] sm:top-[55%] sm:right-10 sm:w-[200px] md:right-15 md:top-[50%] lg:top-[48%] lg:group-hover:top-[52%] lg:right-35 md:w-[250px] xl:top-[45%] xl:w-[300px] xl:group-hover:top-[50%] 2xl:top-[35%] 2xl:w-[447px] 2xl:group-hover:top-[45%]"
+              />
+            </div>
           </div>
         </div>
       </div>
