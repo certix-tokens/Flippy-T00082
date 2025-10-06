2c2aefb8e3750a67a661fe3de4b74d1641128ee3
ravindu-kandawela1205 <ravindu.l@botcalm.com>
2025-10-06 13:18:16 +0530

    fix(UI): fix howtobuy page respnsive and positions

--- src/pages/Howtobuy/sections/Howtobuy.tsx
+++ src/pages/Howtobuy/sections/Howtobuy.tsx
@@ -1,38 +1,43 @@
 import Howtobuybg from '../../../assets/Images/howtobuy.png';
 import pengugif from '../../../assets/Images/P1-v2.gif';
 import { motion } from 'framer-motion';
-
+import { useState } from 'react';
 function HowToBuy() {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const toggleOpacity = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
   return (
     <section
       className="relative min-h-screen w-full overflow-visible bg-cover bg-[position:70%_center] md:bg-center"
       style={{ backgroundImage: `url(${Howtobuybg})` }}
     >
-      <div className="pt-[72px] md:pt-[65px] text-center">
-        <h1 className="font-jack text-[40px] font-normal text-white sm:text-[90px]  xl:text-[120px] 2xl:text-[150px] ">
+      <div className="pt-[72px] text-center md:pt-[65px]">
+        <h1 className="font-jack text-[40px] font-normal text-white sm:text-[90px] xl:text-[120px] 2xl:text-[150px]">
           HOW TO BUY
         </h1>
       </div>
 
-      <div className="relative mt-8 md:mt-16 flex items-center justify-center">
+      <div className="mt-8 flex items-center justify-center md:mt-16" onClick={() => toggleOpacity(0)}>
         <div className="grid-col-2 md:grid-row-1 group grid">
-          <div>
-            <p className="h-50 2xl:h-60 w-[260px] 2xl:w-[460px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] 2xl:px-12  px-10 py-8 text-left text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 mt-20 2xl:-ml-40 md:mt-10 sm:mt-2 xl:mt-2">
-           <ol className='list-disc space-y-4 font-jel 2xl:text-[20px]'>
-            <li>Make Wallet</li>
-            <li>Get some eth</li>
-            <li>swich eth for & Telang</li>
-            <li>Go to uniswap</li>
-           </ol>
-            </p>
+          <div className="flex justify-center">
+            <div className={`absolute top-[25%] xl:left-100 2xl:left-160 2xl:top-[35%] xl:top-[30%] h-50 w-[260px] rounded-2xl bg-gradient-to-b from-[#4B181B] from-30% via-[#9B282F] to-[#B13940] px-10 py-8 text-left text-white sm:mt-2 md:mt-10 xl:mt-2 2xl:-ml-40 2xl:h-60 2xl:w-[460px] 2xl:px-12 ${activeIndex === 0 ? 'opacity-100' : 'opacity-0'}  `}>
+              <ol className="font-jel list-disc space-y-4 2xl:text-[20px]">
+                <li>Make Wallet</li>
+                <li>Get some eth</li>
+                <li>swich eth for & Telang</li>
+                <li>Go to uniswap</li>
+              </ol>
+            </div>
           </div>
 
-          <div>
+          <div className="flex justify-center">
             <motion.img
               src={pengugif}
               alt="How to buy illustration"
               loading="lazy"
-              className=" absolute top-70 md:top-60 2xl:group-hover:ml-40  sm:top-50 h-auto max-w-full group-hover:scale-90 2xl:-mt-10 xl:top-50 w-[280px]"
+              className="absolute top-[45%] left-1/2 z-0 h-auto w-[243px] max-w-full -translate-x-1/2 transform group-hover:scale-90 md:top-[48%] 2xl:top-[55%]"
               initial={{ x: '50vw', opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               transition={{
